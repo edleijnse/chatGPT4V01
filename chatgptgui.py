9b3fdf6196377c981775003359bc6c27faa2fe4c
edleijnse <ed@leijnse.info>
2023-12-08 13:55:35 +0100

    Hints from AI Assistent: Refactoring chatgptui.py

--- chatgptgui.py
+++ chatgptgui.py
@@ -4,10 +4,22 @@ from openai import OpenAI
 client = OpenAI()
 import json
 
+
+class GPTAPI:
+    def generate_answer(self, question):
+        messages = [{"role": "user", "content": question}]
+        try:
+            response = client.chat.completions.create(model="gpt-4", messages=messages)
+            return response.choices[0].message.content
+        except Exception as e:
+            return str(e)
+
+
 class GPTGUI:
     def __init__(self, root):
         self.root = root
         self.root.title("GPT-4 GUI")
+        self.api = GPTAPI()
 
         self.input_label = tk.Label(root, text="Enter your question:")
         self.input_label.pack()
@@ -21,26 +33,25 @@ class GPTGUI:
         self.output_field = tk.Text(root, height=30, width=80)
         self.output_field.pack()
 
-        self.generate_button = tk.Button(root, text="Generate Answer", command=self.generate_answer)
+        self.generate_button = tk.Button(root, text="Generate Answer", command=self.update_answer)
         self.generate_button.pack()
 
-        self.clear_button = tk.Button(root, text="Clear Fields", command=self.clear_fields)  # New clear button
+        self.clear_button = tk.Button(root, text="Clear Fields", command=self.clear_fields)
         self.clear_button.pack()
 
-    def generate_answer(self):
+        # Adding Close button
+        self.close_button = tk.Button(root, text="Close", command=self.root.quit)
+        self.close_button.pack()
+
+    def update_answer(self):
         question = self.input_field.get("1.0", tk.END).strip()
-        messages = [{"role": "user", "content": question}]
-        try:
-            response = client.chat.completions.create(model="gpt-4", messages=messages)
-            response_message = response.choices[0].message.content
-            answer = response_message
-        except Exception as e:
-            answer = str(e)
-        self.output_field.delete('1.0', tk.END)  # Clear previous output
+        answer = self.api.generate_answer(question)
         self.output_field.insert(tk.END, answer)
-    def clear_fields(self):  # New method to clear fields
+
+    def clear_fields(self):
         self.input_field.delete('1.0', tk.END)
         self.output_field.delete('1.0', tk.END)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
